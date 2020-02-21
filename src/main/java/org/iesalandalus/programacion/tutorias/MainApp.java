cc80f07e0ebd088e558154759604b4bd59a7c2c0
Natii <Natii@DESKTOP-7K17A8A>
2020-02-21 12:59:28 +0100

    Modificación de lcase MainApp

--- src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
@@ -1,9 +1,21 @@
 package org.iesalandalus.programacion.tutorias;
 
+import org.iesalandalus.programacion.tutorias.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.tutorias.mvc.controlador.IControlador;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.FactoriaFuenteDatos;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.IModelo;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.Modelo;
+import org.iesalandalus.programacion.tutorias.mvc.vista.FactoriaVista;
+import org.iesalandalus.programacion.tutorias.mvc.vista.IVista;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("Gestión de tutorías del IES Al-Ándalus");
+		IModelo modelo = new Modelo(FactoriaFuenteDatos.MEMORIA.crear());
+		IVista vista = FactoriaVista.TEXTO.crear();
+		IControlador controlador = new Controlador(modelo, vista);
+		controlador.comenzar();
 	}
 
 }
